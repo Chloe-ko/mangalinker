a0b0df31ae8c7295ad72a7655413a616b3c9edaa
Chloe-ko <neneko@nyu.moe>
2023-11-21 10:47:10 +0000

    Make naming more modular

--- main.py
+++ main.py
@@ -171,8 +171,11 @@ def process_file(source_file_path):
     except:
         logger.warning(f"Failed to chown and chmod target subfolder: {target_subfolder_path}")
 
-    target_filename = f"{subfolder_name} ch.{chapter}"
-    if volume:
+    target_filename = f"{subfolder_name}" if os.getenv('INCLUDE_SERIES_IN_FILENAME', True) else ""
+    if volume and os.getenv('INCLUDE_VOLUME', False) and os.getenv('VOLUME_FIRST', True):
+        target_filename += f" vol.{volume}"
+    target_filename += f" ch.{chapter}"
+    if volume and os.getenv('INCLUDE_VOLUME', False) and not os.getenv('VOLUME_FIRST', True):
         target_filename += f" vol.{volume}"
     target_filename += os.path.splitext(filename)[1]
 
