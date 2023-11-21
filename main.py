bcc9f39605c293fb318947b7a89f3fffad5ac036
Chloe-ko <neneko@nyu.moe>
2023-11-21 19:20:49 +0000

    Adjust naming

--- main.py
+++ main.py
@@ -171,12 +171,12 @@ def process_file(source_file_path):
     except:
         logger.warning(f"Failed to chown and chmod target subfolder: {target_subfolder_path}")
 
-    target_filename = f"{subfolder_name}" if os.getenv('INCLUDE_SERIES_IN_FILENAME', True) else ""
+    target_filename = f"{subfolder_name} " if os.getenv('INCLUDE_SERIES_IN_FILENAME', True) else ""
     if volume and os.getenv('INCLUDE_VOLUME', False) and os.getenv('VOLUME_FIRST', True):
-        target_filename += f" volume {volume}"
-    target_filename += f" chapter {chapter}"
+        target_filename += f"Volume {volume} "
+    target_filename += f"Chapter {chapter}"
     if volume and os.getenv('INCLUDE_VOLUME', False) and not os.getenv('VOLUME_FIRST', True):
-        target_filename += f" volume {volume}"
+        target_filename += f" Volume {volume}"
     target_filename += os.path.splitext(filename)[1]
 
     target_file_path = os.path.join(target_subfolder_path, target_filename)
